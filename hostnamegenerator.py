614d03c24b5d8adeddfebe59b6b8bcbb97175404
Michael Peters <michael@michaelpeterswa.com>
2021-03-16 10:36:28 -0700

    feat: core functionality works

--- hostnamegenerator.py
+++ hostnamegenerator.py
@@ -2,8 +2,10 @@
 
 import os
 import inquirer
+import logging
 from pyfiglet import Figlet
 import json
+from tabulate import tabulate
 
 clear = lambda: os.system("clear")
 
@@ -20,22 +22,75 @@ def intro():
     ]
     return inquirer.prompt(questions)
 
-def load_data():
+def load_distance_data():
     with open("data/sorted_by_distance.json", "r") as data:
        return json.load(data)
 
-def view():
-    for line in data[:10]:
-        print(line["name"])
+def view(data):
+    headers = 'keys'
+    if not data:
+        print("No hostnames assigned.\n")
+    elif data:
+        print(tabulate(data, headers), "\n")
+    else:
+        logging.warning("Shouldn't be possible to get here. Exiting!")
+        exit(1)
+
+    loop_to_main()
 
 def create():
-    print('create')
+    curr = [data["hostname"] for data in current_associations]
+    top_ten = [data["name"] for data in distance_data[:10+len(curr)] if data["name"] not in curr]
+    questions = [
+        inquirer.List('hostname',
+                message="Select a hostname",
+                choices=top_ten,
+            ),
+        inquirer.Text('ip',
+                    message="What IP will you associate with {hostname}?"),
+        inquirer.Text('description',
+                    message="Please enter a short description")
+    ]
+
+    return inquirer.prompt(questions)
 
 def edit():
-    print('edit')
+    choice = [data["hostname"] for data in current_associations]
+    questions = [
+        inquirer.List('hostname',
+                message="Select a hostname",
+                choices=choice,
+            ),
+        inquirer.Text('ip',
+                    message="What IP will you associate with {hostname}?"),
+        inquirer.Text('description',
+                    message="Please enter a short description")
+    ]
+
+    new_answer = inquirer.prompt(questions)
+
+    for i, assoc in enumerate(current_associations):
+        if assoc["hostname"] == new_answer["hostname"]:
+            current_associations[i] = new_answer
 
 def delete():
-    print('delete')
+    choice = [data["hostname"] for data in current_associations]
+    questions = [
+        inquirer.List('hostname',
+                message="Select a hostname",
+                choices=choice,
+            )
+    ]
+
+    new_answer = inquirer.prompt(questions)
+
+    for i, assoc in enumerate(current_associations):
+        if assoc["hostname"] == new_answer["hostname"]:
+            del current_associations[i]
+
+def loop_to_main():
+    input("Press Enter to Continue...")
+    main()
 
 def main():
     clear()
@@ -45,11 +100,31 @@ def main():
     clear()
     title()
     if intro_answers["result"] == "View":
-        view()
+        view(current_associations)
+
+    elif intro_answers["result"] == "Create":
+        current_associations.append(create())
+        loop_to_main()
+    
+    elif intro_answers["result"] == "Edit":
+        edit()
+        loop_to_main()
+    
+    elif intro_answers["result"] == "Delete":
+        delete()
+        loop_to_main()
         
     elif intro_answers["result"] == "Quit":
+        print("Exiting!\n")
         exit(0)
+    
+    else:
+        logging.warning("Shouldn't be possible to get here. Exiting!")
+        exit(1)
+
 
 if __name__ == "__main__":
-    data = load_data()
+    current_associations = []
+    logging.basicConfig(level=logging.DEBUG)
+    distance_data = load_distance_data()
     main()
